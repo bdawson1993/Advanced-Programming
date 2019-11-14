3d0b6ec9ead2f440d5fb06e7e26c0dd5953c4f59
Ben Dawson <17676646+bdawson1993@users.noreply.github.com>
2019-11-14 01:19:23 +0000

    Added ball class

---
+++ src/Ball.cpp
@@ -0,0 +1,63 @@
+#include "Ball.h"
+
+void Ball::ApplyFrictionForce(int ms)
+{
+	if (velocity.Magnitude() <= 0.0)
+	{
+		return;
+	}
+
+	vec2 acceleration = -velocity.Normalised();
+	acceleration *= (0.03f * 9.8f);
+	vec2 velocityChange = ((acceleration * ms)) / 1000.0f;
+
+	if (velocityChange.Magnitude() > velocity.Magnitude())
+	{
+		velocity = 0.0;
+	}
+	else
+	{
+		velocity += velocityChange;
+	}
+}
+
+Ball::Ball() : IGameObject()
+{
+	posistion(1) = 0.9;
+}
+
+Ball::~Ball()
+{
+}
+
+void Ball::ApplyForce(vec2 force)
+{
+	vec2 imp((-sin(0.0) * 0.25 * 8.0),
+		(-cos(0.0) * 0.25 * 8.0));
+	velocity = force;
+}
+
+void Ball::Start()
+{
+	
+}
+
+void Ball::Update(int ms)
+{
+	cout << velocity(0) << " " << velocity(1) << endl;
+	ApplyFrictionForce(ms);
+	posistion += ((velocity * ms) / 1000.0f);
+
+	if (velocity.Magnitude() < 0.01f)
+	{
+		velocity = 0.0f;
+	}
+
+}
+
+void Ball::Draw()
+{
+	glTranslatef(posistion(0), (radius/2.0), posistion(1));
+		glutSolidSphere(radius, 32, 32);
+	glColor3f(0, 0, 1.0);
+}
---
+++ src/Ball.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "IGameObject.h"
+#include <iostream>
+
+using namespace std;
+
+class Ball : public IGameObject
+{
+private:
+	float radius = 0.05f;
+	vec2 velocity = vec2(0);
+
+	void ApplyFrictionForce(int ms);
+
+public:
+	Ball();
+	~Ball();
+
+	void ApplyForce(vec2 force);
+
+	// Inherited via IGameObject
+	virtual void Start() override;
+
+	virtual void Update(int ms) override;
+
+	virtual void Draw() override;
+
+};
+
